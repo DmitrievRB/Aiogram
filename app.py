0a6145d17a4026e5ab7ff5a90748eb356b055ecd
DmitrievRB <merlinrd@yandex.ru>
2024-07-10 17:49:44 +0300

    мелкие исправления

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from handlers.admin_private import admin_router
-from handlers.user_group import user_group_router
-from handlers.user_private import user_private_router
-from database.engine import create_db, drop_db, session_maker
 from middlewares.db import DataBaseSession
+from database.engine import create_db, drop_db, session_maker
+from handlers.user_private import user_private_router
+from handlers.user_group import user_group_router
+from handlers.admin_private import admin_router
 import asyncio
 import os
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 from dotenv import find_dotenv, load_dotenv
 
@@ -19,7 +20,8 @@ load_dotenv(find_dotenv())
 
 # ALLOWED_UPDATES = ['message', 'edited_message', 'callback_query']
 
-bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)
+bot = Bot(token=os.getenv('TOKEN'),
+          default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 bot.my_admins_list = []
 
 dp = Dispatcher()
